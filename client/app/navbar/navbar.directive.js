e5d2648d301a6d50882fd7a49c907bc02a2813d7
Edward Russell <elr11789@gmail.com>
2015-01-07 17:48:57 +0000

    navbar first pass

--- client/app/navbar/navbar.directive.js
+++ client/app/navbar/navbar.directive.js
@@ -5,15 +5,21 @@ angular.module('lumbajackApp')
     return {
       templateUrl: 'app/navbar/navbar.html',
       restrict: 'EA',
-      link: function (scope, $location) {
+      link: function (scope, elem, attr, $location) {
+        var navButton = angular.element('.menu-icon');
+
       	scope.menu = [{
       		'title': 'Home',
       		'link': '/'
-    	}];
-    	scope.isCollapsed = true;
-    	scope.isActive = function(route) {
-	      return route === $location.path();
-	    };
+    	  }];
+      	scope.isCollapsed = true;
+      	scope.isActive = function(route) {
+  	      return route === $location.path();
+  	    };
+
+        navButton.on('click', function(){
+          navButton.toggleClass('open');
+        });
       }
     };
   });

d04749766a5bf053955a1f5c8de4c6baf7f69d7f
Edward Russell <elr11789@gmail.com>
2015-01-13 09:50:14 +0000

    mail sending correctly

--- server/api/contact/contact.controller.js
+++ server/api/contact/contact.controller.js
@@ -11,23 +11,24 @@ var transporter = nodemailer.createTransport({
 
 exports.send = function(req,res){
 
-  console.log(req.headers);
-  console.log(req.body);
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.write(JSON.stringify({ status: 'OK' }));
-  res.end();
-  // var emailData = req.body.emailData,
-  // mailOptions = {
-  //   to: 'edward.russell@evident-legal.com',
-  //   subject: 'New request on lumbajack from ',
-  //   from: emailData.from,
-  //   html: emailData.body
-  // };
-  // transporter.sendMail(mailOptions, function(err, info){
-  //   if (err) {
-  //     console.log(err);
-  //   }else{
-  //     console.log('Message sent: ' + info.response);
-  //   }
-  // });
+  // console.log(req.headers);
+  // console.log(req.body);
+  // res.writeHead(200, { 'Content-Type': 'application/json' });
+  // res.write(JSON.stringify({ status: 'OK' }));
+  // res.end();
+  var mailOptions = {
+    to: 'edward.russell@evident-legal.com',
+    subject: 'New request on lumbajack from ',
+    from: req.body.from,
+    html: req.body.body
+  };
+  transporter.sendMail(mailOptions, function(err, info){
+    if (err) {
+      console.log(err);
+      res.json(500, err);
+    }else{
+      console.log('Message sent: ' + info.response);
+      res.json(201, info.response);
+    }
+  });
 }

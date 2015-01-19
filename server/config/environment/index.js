2a3a057ed463895760424d988b901b950d01439b
Edward Russell <elr11789@gmail.com>
2015-01-19 16:01:43 +0000

    all mongo and empty routes removed

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -30,16 +30,7 @@ var all = {
   },
 
   // List of user roles
-  userRoles: ['guest', 'user', 'admin'],
-
-  // MongoDB connection options
-  mongo: {
-    options: {
-      db: {
-        safe: true
-      }
-    }
-  },
+  userRoles: ['guest', 'user', 'admin']
 
 };
 

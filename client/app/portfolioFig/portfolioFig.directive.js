cd9bd957a96efed8c9af80276ddba3a8fe30d0e7
Edward Russell <elr11789@gmail.com>
2015-01-26 17:36:52 +0000

    initial working order, need to sort touch

--- client/app/portfolioFig/portfolioFig.directive.js
+++ client/app/portfolioFig/portfolioFig.directive.js
@@ -5,7 +5,21 @@ angular.module('lumbajackApp')
     return {
       templateUrl: 'app/portfolioFig/portfolioFig.html',
       restrict: 'EA',
+      scope: {
+        image: '@',
+        alt: '@',
+        title:'@',
+        subtitle:'@',
+        link:'@'
+      },
       link: function (scope, element, attrs) {
+        scope.figimage = '/assets/images/' + scope.image;
+
+        var figure = angular.element('.cs-style-3').find('figure');
+
+        figure.on('touchend', function(){
+          figure.toggleClass('cs-hover');
+        });
       }
     };
   });

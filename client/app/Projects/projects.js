84147617ef568bfc8c40a5e67d683387518961b4
Edward Russell <elr11789@gmail.com>
2015-02-03 10:30:24 +0000

    styles for list

--- client/app/Projects/projects.js
+++ client/app/Projects/projects.js
@@ -6,9 +6,9 @@ angular.module('lumbrjackApp')
       .state('projects', {
         url: '/projects',
         templateUrl: 'app/projects/projects.html',
-        controller: 'ProjectsCtrl'
+        controller: 'ProjectsCtrl as '
       })
-      .state('projects.stl', {
+      .state('stl', {
       	url: '/simplify-the-law',
       	templateUrl: 'app/projects/project-views/stl.html'
       })
@@ -20,7 +20,7 @@ angular.module('lumbrjackApp')
       	url: '/right-call',
       	templateUrl: 'app/projects/project-views/rc.html'
       })
-      .state('projects.fsc', {
+      .state('fsc', {
       	url: '/frith-street',
       	templateUrl: 'app/projects/project-views/fsc.html'
       });

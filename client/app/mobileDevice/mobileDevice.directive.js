34bee7fd1c393da300fb73f7025f8f6d6bb6908f
Edward Russell <elr11789@gmail.com>
2015-02-04 14:31:08 +0000

    mobile devices updated so any type/colour can be chosen

--- client/app/mobileDevice/mobileDevice.directive.js
+++ client/app/mobileDevice/mobileDevice.directive.js
@@ -7,11 +7,37 @@ angular.module('lumbrjackApp')
       restrict: 'EA',
       scope: {
       	mobileImage: '@',
-      	mobileAlt: '@'
+      	mobileAlt: '@',
+        deviceType: '@',
+        colour: '@',
+        orientation: '@'
       },
       link: function (scope) {
+
+        var type = scope.deviceType || 'iphone6';
+        var topBar=false, sleep=false, home=false, bottomBar=false, volume=false, sensor=false, speaker=false;
+
+        if (type !== 'ipad') {topBar=true;}
+        if (type !== 'ipad' && type !== 'htc-one' && type !== 'lumia920') {sleep=true;}
+        if (type !== 'ipad' && type !== 'htc-one' && type !== 's5') {volume=true;}
+        if (type !== 'ipad' && type !== 'nexus5' && type !== 'lumia920') {sensor=true;}
+        if (type !== 'ipad' && type !== 'nexus5') {speaker=true;}
+        if (type !== 'nexus5' && type !== 'lumia920' && type !== 'htc-one') {home=true;}
+        if (type !== 'ipad' && type !== 's5' && type !== 'nexus5' &&
+            type !== 'lumia920' && type !== 'htc-one') {bottomBar=true;}
+
+        // Public
+
       	scope.image = '/assets/images/' + scope.mobileImage;
       	scope.alt = scope.mobileAlt;
+        scope.type = type;
+        scope.topBar = topBar;
+        scope.sleep = sleep;
+        scope.home = home;
+        scope.bottomBar = bottomBar;
+        scope.volume = volume;
+        scope.sensor = sensor;
+        scope.speaker = speaker;
       }
     };
   });

c9fbd63be6df908253a8eb49cab56806d8fd2cda
Edward Russell <elr11789@gmail.com>
2015-02-16 17:39:35 +0000

    homepage updates

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -3,9 +3,20 @@
 angular.module('lumbrjackApp')
   .controller('MainCtrl', function () {
   	var ctrl = this;
-    this.tapHover = tapHover;
+
+    ctrl.tapHover = tapHover;
+    ctrl.posts = [
+      {title: 'Building a site pt. 1', subtitle: 'For this lumbrjack\'s inaugural posts it seemed like a good idea to describe the processes and gotchas of building my portfolio site. This will be a catharsis for my brain, and a chance to practice my writing skills. And it may just provide a bit of guidance and instruction to someone in a similar position!'},
+      {title: 'Building a site pt. 2', subtitle: 'For this lumbrjack\'s inaugural posts it seemed like a good idea to describe the processes and gotchas of building my portfolio site. This will be a catharsis for my brain, and a chance to practice my writing skills. And it may just provide a bit of guidance and instruction to someone in a similar position!'},
+      {title: 'Building a site pt. 3', subtitle: 'For this lumbrjack\'s inaugural posts it seemed like a good idea to describe the processes and gotchas of building my portfolio site. This will be a catharsis for my brain, and a chance to practice my writing skills. And it may just provide a bit of guidance and instruction to someone in a similar position!'},
+      {title: 'Building a site pt. 4', subtitle: 'For this lumbrjack\'s inaugural posts it seemed like a good idea to describe the processes and gotchas of building my portfolio site. This will be a catharsis for my brain, and a chance to practice my writing skills. And it may just provide a bit of guidance and instruction to someone in a similar position!'},
+      {title: 'Building a site pt. 5', subtitle: 'For this lumbrjack\'s inaugural posts it seemed like a good idea to describe the processes and gotchas of building my portfolio site. This will be a catharsis for my brain, and a chance to practice my writing skills. And it may just provide a bit of guidance and instruction to someone in a similar position!'},
+      {title: 'Building a site pt. 6', subtitle: 'For this lumbrjack\'s inaugural posts it seemed like a good idea to describe the processes and gotchas of building my portfolio site. This will be a catharsis for my brain, and a chance to practice my writing skills. And it may just provide a bit of guidance and instruction to someone in a similar position!'},
+    ];
 
     function tapHover ($event){
       $event.element.toggleClass('skill-hover');
     };
+
+
   });
